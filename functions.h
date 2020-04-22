e9c01c475218630c183592a1e1496b7fdd3e019a
szhan89 <zhangsiweiv@gmail.com>
2020-04-22 18:12:40 -0400

    First Commit
    This commit include updates that allow user to enter a zip code and download a xml file, in which contain 5 days weather information for every 3 hours in that area.

---
+++ functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+void APIcall(char *zipcode);
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"functions.h"
+
+int main(){
+    char *usr_zipcode = malloc(5 * sizeof(*usr_zipcode));
+    printf("Welcome to Weather Tracker\nPlease Enter Your Area Zipcode:");
+    scanf("%s",usr_zipcode);//Store user entered zipcode
+    APIcall(usr_zipcode);//Make API call and generate a xml file with the location information
+    return 0;
+}
