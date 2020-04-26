3af2eb60028bc1d7c70f09d70119898dd1536f4d
Kali <chen3066@purdue.edu>
2020-04-26 18:28:53 -0400

    Modifications to file and display data

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include<stdlib.h>
 #include<string.h>
 #include"functions.h"
+#include"cJSON.h"
 
 int main(){
   char *usr_zipcode = malloc(5 * sizeof(*usr_zipcode));
   printf("Welcome to Weather Tracker\nPlease Enter Your Area Zipcode:");
    scanf("%s",usr_zipcode);//Store user entered zipcode
    APIcall(usr_zipcode);//Make API call and generate a xml file with the location information
-   readJson("1");
+   readJson();
+   DisplayData();
     return 0;
+   
+  
 }
--- readJson.c
+++ readJson.c
@@ -30,32 +30,41 @@ struct forecast{//different times of a location
     
 };
 
+
+
 struct cityInformation{//the information about a location
     char* cityName;
     char* Country;
     int timeZone;
     struct forecast forecastInformation[40];//contain 40 different times
 };
-char* getTimezone(int a){//convert the time zone from seconds to hour
+
+struct cityInformation cI;//createing a city structure
+
+ char c[10] = "GMT";
+ 
+int getTimezone(int a){//convert the time zone from seconds to hour
     a= a/3600;
-    char c[10] = "GMT";
     
+    return a;
+   /* 
     char b[5];
     
     sprintf(b, "%d",a);
     
     strcat(c, b);
     // printf("%s",c);
-    return c;
+  
+    */
 }
 void readJson(){
     
-    struct cityInformation cI;//createing a city structure
+    
     char jsonString[100000]="";//set string for the json
     FILE *jsonFile;//create a json file
     char ch;
     //2
-    jsonFile = fopen("weather.json", "r");//read the json file
+    jsonFile = fopen("weather.Json", "r");//read the json file
     //3
     if (jsonFile == NULL)
     {
@@ -120,9 +129,85 @@ void readJson(){
         cI.forecastInformation[i] = Forecast;
         
         
-        
     }
-    
+
+}
+
+void DisplayData()
+{
+	 int i, tzone = 0;
+	 double temp;
+
+	 printf("Location Information\t  City name: %-20s", cI.cityName); //printf statements to display location information
+	 printf("Country: %-10s", cI.Country);
+	 tzone = getTimezone(cI.timeZone);
+	 printf("TimeZone: GMT%d:00\n\n", tzone);
+	 
+	 printf("Date/Time:      ");
+	 
+	 for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-25s", cI.forecastInformation[i].time);
+	 }
+	 
+	 printf("\nWeather:        ");
+	 
+	 for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-25s", cI.forecastInformation[i].weatherDes);
+	 }
+
+	 
+	 printf("\nTemperature:    ");
+	   
+	  for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	temp = cI.forecastInformation[i].temperature;
+	 	temp = temp - 273.15; //convert kelvin to celsius
+	 	printf("%-3.0f°C                    ", temp);
+	 }
+	 
+	 printf("\nFeels Like:     ");
+	 
+	 for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	temp = cI.forecastInformation[i].feelsLike;
+	 	temp = temp - 273.15;
+	 	printf("%-3.0f°C                    ", temp);
+	 }
+	 
+	 printf("\nHumidity(%):    ");
+	 
+	  for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-3i                      ", cI.forecastInformation[i].humidity);
+	 	
+	 }
+	 
+	 printf("\nPressure:       ");
+	 
+	 for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-5.0fhPa                 ", cI.forecastInformation[i].pressure);
+	 }
+	 
+	 
+	 printf("\nWindSpeed:      ");
+	 
+	  for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-5.2fm/h                 ", cI.forecastInformation[i].windSpeed);
+	 }
+	 
+	 printf("\nWindDirection:  ");
+	 
+	 for(int i=0;i<5;i++) //for loop to display contents of array
+	 {
+	 	printf("%-3.0f°                     ", cI.forecastInformation[i].windDirection);
+	 }
+	 printf("\n");
+	
+	
 }
 
 
