07b26aca9120aa970594ed7bf1b3d8b15d0c48d6
chen3066 <49539919+chen3066@users.noreply.github.com>
2020-04-25 20:54:11 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 #include"functions.h"
+#include"cJSON.h"
 
 int main(){
-    char *usr_zipcode = malloc(5 * sizeof(*usr_zipcode));
-    printf("Welcome to Weather Tracker\nPlease Enter Your Area Zipcode:");
-    scanf("%s",usr_zipcode);//Store user entered zipcode
-    APIcall(usr_zipcode);//Make API call and generate a xml file with the location information
+  char *usr_zipcode = malloc(5 * sizeof(*usr_zipcode));
+  printf("Welcome to Weather Tracker\nPlease Enter Your Area Zipcode:");
+   scanf("%s",usr_zipcode);//Store user entered zipcode
+   APIcall(usr_zipcode);//Make API call and generate a xml file with the location information
+   readJson();
     return 0;
+   
+  
 }
